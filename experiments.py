2ddd6cb11db43e9fbbdf5552a35bd709a2fcac67
Nithin <nithintumma@gmail.com>
2016-04-01 00:04:32 -0400

    km

--- experiments.py
+++ experiments.py
@@ -550,7 +550,8 @@ def kmeans_experiment(on_sketch=True, on_orig=True):
     mat = load_matrix('data_batch_1')
     # sketch the transpose 
     mat = mat.T
-    sketch_sizes = [10, 50, 100, 200]
+    #sketch_sizes = [10, 50, 100, 200]
+    sketch_sizes = [10, 30, 50, 100]
     sketches = []
     for l in sketch_sizes:
         fname = 'test_matrices/sketches/sk_%d'%l
